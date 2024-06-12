8ee9d4672645a61f2858aadcf011819d4014b79c
daddyjanno <jn.drugmand@gmail.com>
2024-06-12 12:27:48 +0200

    prisma in lib

---
+++ src/lib/prisma.ts
@@ -0,0 +1,13 @@
+// Disable TypeScript to avoid troubles with `global.` and avoid vscode import troubles
+
+// @ts-nocheck
+
+import { PrismaClient } from "@prisma/client"
+
+const prisma: PrismaClient = global.prisma ?? new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
+})
+
+if (process.env.NODE_ENV !== "production") {
+    global.prisma = prisma
+}
