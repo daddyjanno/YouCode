246f3ec848d52169b0cb20b95b87520b9c52c8eb
daddyjanno <jn.drugmand@gmail.com>
2024-06-14 09:55:47 +0200

    AuthButton

--- src/features/auth/AuthButton.tsx
+++ src/features/auth/AuthButton.tsx
@@ -1,18 +1,17 @@
 import { getAuthSession } from '@/lib/auth'
 import { LoggedInButton } from './LoggedInButton'
 import { LoginButton } from './LoginButton'
-import { getSession } from 'next-auth/react'
 
 export type AuthButtonProps = {}
 
 export const AuthButton = async (props: AuthButtonProps) => {
     const session = await getAuthSession()
+    const user = session?.user
     console.log(session)
-    return <LoginButton />
 
-    // if (!user) {
-    //     return <LoginButton />
-    // }
+    if (!user) {
+        return <LoginButton />
+    }
 
-    // return <LoggedInButton user={session} />
+    return <LoggedInButton user={user} />
 }
